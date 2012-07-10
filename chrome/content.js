7e831a422f4b445950ecd02dd5c12b07747f88bb
Philipp Bosch <hello@pb.io>
2012-07-10 15:25:30 +0200

    Use jQuery.

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,9 @@
-var replTemplate = document.createElement('div');
-replTemplate.setAttribute('class', 'ad-ade-repl');
-replTemplate.innerHTML = '<svg version="1.1" width="100%" height="100%" viewBox="0 0 100 100"><circle fill="#FF0000" cx="50" cy="50" r="50"/></svg>';
+var $replTemplate = $('<div class="ad-ade-repl"></div>'),
+    $circle = $('<svg version="1.1" width="100%" height="100%" viewBox="0 0 100 100"><circle fill="#FF0000" cx="50" cy="50" r="50"/></svg>'),
+    $body = $('body');
+
+$circle.appendTo($replTemplate);
+$replTemplate.on('click', openLightbox);
 
 var openLightbox = function() {
     alert('CLICK!');
@@ -9,32 +12,36 @@ var openLightbox = function() {
 var applyFilters = function() {
     chrome.extension.sendMessage({'action': 'getSelectors'}, function(selectors) {
         selectors.forEach(function(selector) {
-            var elements = document.querySelectorAll(selector);
-            if (elements.length) {
-                for (var i=0, l=elements.length; i<l; i++) {
-                    var element = elements[i],
-                        rect = element.getBoundingClientRect(),
-                        repl = replTemplate.cloneNode(true);
-                    repl.style.position = 'absolute';
-                    repl.style.left = element.offsetLeft + 'px';
-                    repl.style.top = element.offsetTop + 'px';
-                    repl.style.width = element.offsetWidth + 'px';
-                    repl.style.height = element.offsetHeight + 'px';
-                    repl.style.opacity = 0;
-                    repl.style.webkitTransform = 'rotate(' + (Math.random() * 2 - 1) + 'deg)';
-                    repl.addEventListener('click', openLightbox);
-                    document.body.appendChild(repl);
-                    setTimeout(function(repl) {
-                        return function() { repl.style.opacity = 1; }
-                    }(repl), 1);
-                }
+            var $elements = $(selector);
+            if ($elements.length) {
+                $elements.each(function(i, element) {
+                    var $element = $(element);
+                    console.log(1, element, $element.position().top);
+                    setTimeout(function($element) {
+                        return function() {
+                            console.log(2, element, $element.position().top);
+                        }
+                    }($element), 1000);
+                    var $repl = $replTemplate.clone(true);
+                    $repl.css({
+                        position: 'absolute',
+                        left: element.offsetLeft + 'px',
+                        top: element.offsetTop + 'px',
+                        width: element.offsetWidth + 'px',
+                        height: element.offsetHeight + 'px',
+                        opacity: 0 /*,
+                        webkitTransform: 'rotate(' + (Math.random() * 2 - 1) + 'deg)' */
+                    });
+                    $repl.appendTo($body);
+                    setTimeout(function($repl) {
+                        return function() { $repl.css('opacity', 1); }
+                    }($repl), 1);
+                });
             }
         });
     });
 };
 
-if (document.readyState == 'complete') {
+$(document).ready(function() {
     applyFilters();
-} else {
-    window.addEventListener('load', applyFilters);
-}
+});
